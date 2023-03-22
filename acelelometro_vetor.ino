f1cc4abea3280233c998b6ab1b635db607465e5e
Vectorj7632 <tartaruganzxs@gmail.com>
2023-03-22 16:56:29 -0300

    criando variaveis

--- acelelometro_vetor.ino
+++ acelelometro_vetor.ino
@@ -1,3 +1,20 @@
+#incluide <MPU6050_tockn.h>
+#incluide <Wire.h>
+
+#define pinLedX 12
+#define pinLedY 11
+#define pinLedZ 10
+
+#define MPU6050_ADDR 0x68
+
+#define DEBUG
+MPU6050 mpu6050(Wire);
+
+float anguloX;
+float anguloY;
+float anguloZ;
+
+unsigned long controleTempo;
 void setup() {
   // put your setup code here, to run once:
 
